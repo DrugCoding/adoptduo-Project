6e260d3b1cad48a9cc2cfc89f4ea6add7805caf5
DrugCoding <lhs940308@gmail.com>
2022-11-30 17:10:00 +0900

    Add: accounts 기본기능 완료

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,22 +4,42 @@ from django.contrib.auth import get_user_model
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = [
+        fields = (
+            "name",
             "username",
-            "volunteer_c",
             "area",
+            "volunteer_c",
+            "email",
             "password1",
             "password2",
             "image",
-        ]
+        )
         labels = {
+            "name": "이름",
+            "username": "아이디",
+            "area": "거주지역",
             "volunteer_c": "이동봉사 여부",
-            "area": "지역",
+            "email": "이메일",
+            "image": "이미지 파일",
         }
+        # widgets = {
+        #     'username': UserCreationForm(attrs={'placeholder': '추후 수정불가'}),
+        # }
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
-        fields = [
-            'area'
-        ]
+        fields = (
+            "name",
+            "area",
+            "volunteer_c",
+            "email",
+            "image",
+        )
+        labels = {
+            "name": "이름",
+            "area": "거주지역",
+            "volunteer_c": "이동봉사 여부",
+            "email": "이메일",
+            "image": "이미지 파일",
+        }
