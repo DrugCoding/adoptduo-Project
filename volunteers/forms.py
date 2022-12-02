ff935dc87e1806bfab24e0e92b464eb8f483686e
DrugCoding <lhs940308@gmail.com>
2022-12-02 17:25:43 +0900

    Add:B/volunteers, Update:B/account, Update:B/articles/search.html 추가 및 수정

---
+++ volunteers/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from .models import Volunteer, VolunteerComment
+
+class VolunteerForm(forms.ModelForm):
+    class Meta:
+        model = Volunteer
+        fields = [
+            'title',
+            'area',
+            'adopt_location',
+            'adopt_date',
+            'content',
+        ]
+        labels = {
+            'title': '제목',
+            'area': '출발지역',
+            'adopt_location': '도착지역',
+            'adopt_date': '이동날짜',
+            'content': '추가내용',
+        }
+        widgets = {
+            'area': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': '출발하는 지역'
+                }
+            ),
+            'adopt_location': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': '도착하는 지역'
+                }
+            ),
+            'adopt_date': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'yyyy-mm-dd'
+                }
+            ),
+            'content': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': '100글자 제한'
+                }
+            )
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = VolunteerComment 
+        fields = ['content',]
