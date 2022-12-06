a42a959057391114c6a761395ef7d203ebde22c8
tmsnvl95 <tmsnvl95@naver.com>
2022-12-06 17:15:13 +0900

    update: B/notes/쪽지함 구현완료/프사수정중

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,5 @@ app_name = 'notes'
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:pk>/send/", views.send, name="send"),
+    path("<int:pk>/", views.detail, name="detail"),
 ]
