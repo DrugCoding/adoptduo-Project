fc292c74b6050706cef276775d789c0be12d254d
TocDX <sk372021000@gmail.com>
2022-12-09 18:47:37 +0900

    update: F/stories/forms 수정

--- stories/forms.py
+++ stories/forms.py
@@ -1,17 +1,31 @@
 from .models import Stories, StoryComment
 from django import forms
 
+
 class StoriesForm(forms.ModelForm):
     class Meta:
         model = Stories
-        fields = ["title","name", "breed", "content", "image"]
+        fields = ["title", "content", "image"]
         labels = {
-            "title": "제목",
-            "name": "반려동물 이름",
-            "breed": "견종",
-            "content": "내용",
-            "image": "사진",
+            "title": "🗒️제목",
+            "content": "📝내용",
+            "image": "📸사진",
         }
+
+        widgets = {
+            "title": forms.TextInput(
+                attrs={
+                    "class": "hover-control",
+                    "placeholder": "게시글 제목을 입력해주세요.",
+                }),
+
+            "content": forms.TextInput(
+                attrs={
+                    "class": "hover-control",
+                    "placeholder": "게시글을 입력해주세요.",
+                }),
+        }
+
 
 class Stories_CommentForm(forms.ModelForm):
     class Meta:
