576901e35c179334d2818420a5f72ebe620dfcea
tmsnvl95 <tmsnvl95@naver.com>
2022-12-13 11:00:13 +0900

    admin생성중3

--- articles/management/commands/createsu.py
+++ articles/management/commands/createsu.py
@@ -4,6 +4,6 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not get_user_model().objects.filter(username="admin").exists():
-            get_user_model().objects.create_superuser("admin", "admin@admin.com", "admin")
+        if not get_user_model().objects.filter(username="admin1").exists():
+            get_user_model().objects.create_superuser("admin1", "admin@admin.com", "admin1")
             self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
